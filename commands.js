2dcf69761276d927d835606a3377d02b9a6da3b1
LuckyShmucky <christianluciano567@gmail.com>
2022-03-16 16:22:03 -0700

    almost done with the bonus

--- commands.js
+++ commands.js
@@ -50,6 +50,157 @@
 //db.bounties.updateMany({captured: false}, {$set: {captured: true}})
 
 
+///////BONUS
+
+//db.createCollection('scavangers')
+
+//db.scavengers.inseryMany([ {
+  //   "name": "Captain A'Meerkat",
+  //   "joined": new Date('2011-07-22'),
+  //   "power": "Thermoregulation",
+  //   "weapon": "Shovel",
+  //   "captured": [
+  //     {
+  //       "name": "Grim Panda",
+  //       "species": "Giant Panda",
+  //       "location": "Temperate forest",
+  //       "wantedFor": "Eating all the bamboo",
+  //       "client": "Red wolf",
+  //       "reward": 5000,
+  //       "captured": true
+  //     }
+  //   ]
+  // },
+  // {
+  //   "name": "The Hamster",
+  //   "joined": new Date('2011-07-22'),
+  //   "power": "Stealth",
+  //   "weapon": "Giant rolling ball",
+  //   "captured": [
+  //     {
+  //       "name": "Polarwind",
+  //       "species": "Polar Bear",
+  //       "location": "Arctic",
+  //       "wantedFor": "Not hibernating",
+  //       "client": "Sabertooth",
+  //       "reward": 10000,
+  //       "captured": true
+  //     }
+  //   ]
+  // },
+  // {
+  //   "name": "Thowl",
+  //   "joined": new Date('2011-07-22'),
+  //   "power": "Night vision",
+  //   "weapon": "Lasers",
+  //   "captured": [
+  //     {
+  //       "name": "Thanoceros",
+  //       "species": "Rhinoceros",
+  //       "location": "Grasslands",
+  //       "wantedFor": "Eating too much grass",
+  //       "client": "Songbird",
+  //       "reward": 10000,
+  //       "captured": true
+  //     },{
+  //       "name": "Polarwind",
+  //       "species": "Polar Bear",
+  //       "location": "Arctic",
+  //       "wantedFor": "Not hibernating",
+  //       "client": "Sabertooth",
+  //       "reward": 10000,
+  //       "captured": true
+  //     }
+  //   ]
+  // },
+  // {
+  //   "name": "Brown Recluse",
+  //   "joined": new Date('2011-07-22'),
+  //   "power": "Inciting fear into the heart of enemies",
+  //   "weapon": "Webs",
+  //   "captured": [
+  //     {
+  //       "name": "Thanoceros",
+  //       "species": "Rhinoceros",
+  //       "location": "Grasslands",
+  //       "wantedFor": "Eating too much grass",
+  //       "client": "Songbird",
+  //       "reward": 10000,
+  //       "captured": true
+  //     },{
+  //       "name": "Wrecking Crows",
+  //       "species": "Crow",
+  //       "location": "Grasslands",
+  //       "wantedFor": "Cawing too loudly",
+  //       "client": "Red wolf",
+  //       "reward": 40000,
+  //       "captured": true
+  //     }
+  //   ]
+  // },
+  // {
+  //   "name": "Falconeye",
+  //   "joined": new Date('2011-07-22'),
+  //   "power": "Flight",
+  //   "captured": [
+  //     {
+  //       "name": "Wrecking Crows",
+  //       "species": "Crow",
+  //       "location": "Grasslands",
+  //       "wantedFor": "Cawing too loudly",
+  //       "client": "Red wolf",
+  //       "reward": 40000,
+  //       "captured": true
+  //     }
+  //   ]
+  // },
+  // {
+  //   "name": "Scarlet Wolf",
+  //   "joined": new Date('2015-05-01'),
+  //   "power": "Hunting",
+  //   "captured": [
+  //     {
+  //       "name": "Grim Panda",
+  //       "species": "Giant Panda",
+  //       "location": "Temperate forest",
+  //       "wantedFor": "Eating all the bamboo",
+  //       "client": "Red wolf",
+  //       "reward": 5000,
+  //       "captured": true
+  //     }
+  //   ]
+  // },
+  // {
+  //   "name": "Black Jaguar",
+  //   "joined": new Date('2018-04-23'),
+  //   "power": "Camoflauge",
+  //   "captured": [
+  //     {
+  //       "name": "Grim Panda",
+  //       "species": "Giant Panda",
+  //       "location": "Temperate forest",
+  //       "wantedFor": "Eating all the bamboo",
+  //       "client": "Red wolf",
+  //       "reward": 5000,
+  //       "captured": true
+  //     },{
+  //       "name": "Thanoceros",
+  //       "species": "Rhinoceros",
+  //       "location": "Grasslands",
+  //       "wantedFor": "Eating too much grass",
+  //       "client": "Songbird",
+  //       "reward": 10000,
+  //       "captured": true
+  //     }
+  //   ]
+  // }]) 
+
+  //db.scavengers.find({joined: {$gte: new Date('2011-12-31')}})
+
+  //db.scavengers.find({'captured.name': "Thanoceros"}
+
+  //db.scavengers.find({'captured.reward': {$gt: 11000}})
+
 // 2. Create a collection called bounties
 db.createCollection("bounties")
 
